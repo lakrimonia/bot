0fed30fd60efb38c186ded6a666b54b4b29581d6
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-09-15 03:52:50 +0500

    Renaming variables

--- src/com/company/Conversation.java
+++ src/com/company/Conversation.java
@@ -4,31 +4,31 @@ import java.io.*;
 import java.util.HashMap;
 
 public class Conversation {
-    private HashMap<String, String> text;
-    private HashMap<String, String> question_answer;
+    private HashMap<String, String> key_data; //Basic background information
+    private HashMap<String, String> question_answer; 
 
     public Conversation() throws IOException {
-        this.text = new HashMap<>();
-        this.text.put("ПРИВЕТСТВИЕ", null);
-        this.text.put("СПРАВКА", null);
-        this.text.put("ПРЕДЛОЖЕНИЕ ПОИГРАТЬ", null);
+        this.key_data = new HashMap<>();
+        this.key_data.put("ПРИВЕТСТВИЕ", null);
+        this.key_data.put("СПРАВКА", null);
+        this.key_data.put("ПРЕДЛОЖЕНИЕ ПОИГРАТЬ", null);
         this.question_answer = new HashMap<>();
-        String[] text = get_text();
-        for (int i = 0; i < text.length - 1; i++) {
-            if (this.text.containsKey(text[i])) {
-                String key = text[i];
-                StringBuilder text_sb = new StringBuilder();
+        String[] lines = get_text();
+        for (int i = 0; i < lines.length - 1; i++) {
+            if (this.key_data.containsKey(lines[i])) {
+                String key = lines[i];
+                StringBuilder s_builder = new StringBuilder();
                 i++;
-                while (!text[i].equals("")) {
-                    text_sb.append(text[i]);
-                    text_sb.append("\r\n");
+                while (!lines[i].equals("")) {
+                    s_builder.append(lines[i]);
+                    s_builder.append("\r\n");
                     i++;
                 }
-                this.text.put(key, text_sb.toString());
-            } else if (text[i].equals("ВОПРОСЫ")) {
+                this.key_data.put(key, s_builder.toString());
+            } else if (lines[i].equals("ВОПРОСЫ")) {
                 i++;
-                while (!text[i].equals("")) {
-                    String[] question_answer = text[i].split("--");
+                while (!lines[i].equals("")) {
+                    String[] question_answer = lines[i].split("--");
                     this.question_answer.put(question_answer[0], question_answer[1]);
                     i++;
                 }
