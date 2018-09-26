c1a21afae6a64abd0f2240f32add0e821b76c0c3
odinochka3000 <38666896+odinochka3000@users.noreply.github.com>
2018-09-27 00:18:43 +0500

    Update Bot.java
    - topicContent заполняется независимо от кол-ва тем

--- src/com/company/Bot.java
+++ src/com/company/Bot.java
@@ -4,52 +4,49 @@ import java.io.*;
 import java.util.HashMap;
 
 public class Bot {
-    public HashMap<String, String> topicContent;
-    public HashMap<String, String> questionAnswer;
+	public HashMap<String, String> topicContent;
+	public HashMap<String, String> questionAnswer;
 
-    public Bot() throws IOException {
-        topicContent = new HashMap<>();
-        topicContent.put("ПРИВЕТСТВИЕ", null);
-        topicContent.put("СПРАВКА", null);
-        topicContent.put("ПРЕДЛОЖЕНИЕ ПОИГРАТЬ", null);
-        topicContent.put("НЕКОРРЕКТНАЯ КОМАНДА", null);
-        topicContent.put("ПРОЩАНИЕ", null);
-        this.questionAnswer = new HashMap<>();
-        String[] lines = getText();
-        for (int i = 0; i < lines.length - 1; i++) {
-            if (this.topicContent.containsKey(lines[i])) {
-                String topic = lines[i];
-                StringBuilder content = new StringBuilder();
-                i++;
-                while (!lines[i].equals("")) {
-                    content.append(lines[i]);
-                    content.append("\r\n");
-                    i++;
-                }
-                this.topicContent.put(topic, content.toString());
-            } else if (lines[i].equals("ВОПРОСЫ")) {
-                i++;
-                while (!lines[i].equals("")) {
-                    String[] question_answer = lines[i].split("--");
-                    this.questionAnswer.put(question_answer[0], question_answer[1]);
-                    i++;
-                }
-            }
-        }
-    }
+	public Bot() throws IOException {
+		topicContent = new HashMap<>();
+		this.questionAnswer = new HashMap<>();
+		String[] lines = getText("text.txt");
+		for (int i = 0; i < lines.length - 1; i++) {
+			if (lines[i].charAt(0) == '#') {
+				String topic = lines[i].substring(1);
+				StringBuilder content = new StringBuilder();
+				i++;
+				while (!lines[i].equals("")) {
+					content.append(lines[i]);
+					content.append("\r\n");
+					i++;
+				}
+				this.topicContent.put(topic, content.toString());
+			}
+		}
 
-    private String[] getText() throws IOException {
-        File file = new File("text.txt");
-        char[] c = new char[(int) file.length()];
-        Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8");
-        reader.read(c);
-        reader.close();
-        String str = new String(c);
-        return str.split("\r\n");
-    }
+		lines = getText("qus.txt");
+		for (int i = 1; i < lines.length - 1; i++) {
+			while (!lines[i].equals("")) {
+				String[] question_answer = lines[i].split("--");
+				this.questionAnswer.put(question_answer[0], question_answer[1]);
+				i++;
+			}
+		}
+	}
 
-    public void start() {
-        Conversation conversation = new Conversation(topicContent, questionAnswer);
-        conversation.start();
-    }
+	private String[] getText(String name) throws IOException {
+		File file = new File(name);
+		char[] c = new char[(int) file.length()];
+		try (Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8")) {
+			reader.read(c);
+		}
+		String str = new String(c);
+		return str.split("\r\n");
+	}
+
+	public void start() {
+		Conversation conversation = new Conversation(topicContent, questionAnswer);
+		conversation.start();
+	}
 }
