238572eced9571a9bb17b82bf1bfe4d4f7b232ad
lakrimonia <anasaturday@yandex.ru>
2018-10-04 00:00:03 +0500

    Tests

--- src/com/company/ConversationTest.java
+++ src/com/company/ConversationTest.java
@@ -3,5 +3,8 @@ package com.company;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConversationTest {
-
+    @org.junit.jupiter.api.Test
+    void test() {
+        fail();
+    }
 }
--- src/com/company/QuizTest.java
+++ src/com/company/QuizTest.java
@@ -3,5 +3,8 @@ package com.company;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuizTest {
-
+    @org.junit.jupiter.api.Test
+    void test() {
+        fail();
+    }
 }
